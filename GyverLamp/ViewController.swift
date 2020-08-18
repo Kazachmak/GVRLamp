04813b930d8516ec489e2ba71ee43543449d2c6e
Maksim Kazachkov <kazachmak@gmail.com>
2020-08-18 18:33:52 +0300

    Initial Commit

---
+++ GyverLamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GyverLamp
+//
+//  Created by Максим Казачков on 18.08.2020.
+//  Copyright © 2020 Maksim Kazachkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
